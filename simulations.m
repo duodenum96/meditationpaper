9b28e05c927263fe98b8aea17e86c15e0a3c9194
duodenum96 <catalyasir@gmail.com>
2022-12-08 12:32:46 -0500

    Switched to trial based, boxcars taken out

--- simulations.m
+++ simulations.m
@@ -8,101 +8,29 @@ init
 %% Start with colored noise input
 
 ple = 0:0.2:1.6;
-figure;
-for i = 1:length(ple)
-    
-    noisesource = dsp.ColoredNoise(ple(i), ntime, 1);    
-    noise = noisesource();
-    
-    p.I_ext_E  = zeros([29 ntime]);
-    p.I_ext_E(1,:) = noise;
-    
-    [v_E, time] = chaudhuri(p);
-    
-    acw_0 = zeros(1, nrois);
-    for j = 1:size(v_E, 1)
-        acw_0(j) = acw(v_E(j, :), (1/p.dt)*1000);
-    end
-    
-    subplot(3, 3, i)
-    stem(1:nrois, acw_0, 'k', 'filled')
-    if any(i == [1 4 7]), ylabel('ACW-0'),end
-    if any(i == [7 8 9]), xticks(1:nrois), xticklabels(rois), xtickangle(90), else, xticks({}), end
-    ylim([0 6])
-    
-    title(['PLE of external stimulus: ', num2str(ple(i))])
-end
-
-saveas(gcf, [projectfolder, '\figures\noises.jpg'])
-%% Proceed with boxcars with ISIs and durations
-
-isis = [0.5 1 1.5]; % ISIs
-durs = [0.5 1 1.5]; % Durations
-str = 3; % Strength of boxcar
-inputtrange = [54 115];
-ninput = 20;
-p.tspan = inputtrange(2);
-timevector = 0:p.dt:p.tspan/p.dt*1000;
-ntime = length(timevector);
-
-figure;
-c = 1;
-for i = 1:length(isis)
-    for j = 1:length(durs)
-        isi = isis(i);
-        dur = durs(j);
+ntrials = 50;
+acw_0 = zeros(length(ple), nrois, ntrials);
+for c = 1:ntrials
+    tic
+    disp(['Trial ' num2str(c), ' started'])
+    for i = 1:length(ple)
         
-        boxcar = zeros([1 ntime]);
-        noise = randn([1 ntime]);
-        
-        tstart = inputtrange(1);
-        tend = tstart - isi;
-        
-        for m = 1:ninput
-            tstart = tend+isi;
-            tend = tstart + dur;
-            boxcar(tstart*1000:p.dt:tend*1000) = str;
-        end
+        noisesource = dsp.ColoredNoise(ple(i), ntime, 1);
+        noise = noisesource();
         
         p.I_ext_E  = zeros([29 ntime]);
-        p.I_ext_E(1,:) = noise + boxcar;
-    
+        p.I_ext_E(1,:) = noise;
+        
         [v_E, time] = chaudhuri(p);
         
-        acw_0 = zeros(1, nrois);
-        for m = 1:size(v_E, 1)
-            acw_0(m) = acw(v_E(m, :), (1/p.dt)*1000);
+        
+        
+        for j = 1:size(v_E, 1)
+            acw_0(i, j, c) = acw(v_E(j, :), (1/p.dt)*1000);
         end
-        
-        subplot(3, 3, c)
-        
-        stem(1:nrois, acw_0, 'k', 'filled')
-        if any(c == [1 4 7]), ylabel('ACW-0'),end
-        if any(c == [7 8 9]), xticks(1:nrois), xticklabels(rois), xtickangle(90), else, xticks({}), end
-%         ylim([0 5])
-    
-        title(['Duration: ', num2str(dur), ' ISI: ', num2str(isi)])
-        c = c + 1;
     end
+    disp(['Trial ' num2str(c), ' finished'])
+    toc
 end
 
-saveas(gcf, [projectfolder, '\figures\boxcars.jpg'])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+save([projectfolder, '\data\acw_0results.mat'], 'acw_0')
